08d23ccb51c6902c242e274bb8fb0fef06d95dc4
DESKTOP-6S97JAT\Nero <thanyaporn.ch688@gmail.com>
2023-03-11 19:22:59 +0700

    get all user transaction

--- src/pages/OrderStatusPage.js
+++ src/pages/OrderStatusPage.js
@@ -1,8 +1,33 @@
 import FooterUser from '../components/FooterUser';
 import HeaderCart from '../layouts/HeaderCart';
 import lllLogo from '../assets/icons/lll.png';
+import { getAllBillApi } from '../apis/auth-api';
+import useOrder from '../hooks/useOrder';
+import { useEffect, useState } from 'react';
 
-export default function orderStatus() {
+export default function OrderStatusPage() {
+  // const [order,setOrder] = useState()
+  // const fetchOrder = async () => {
+  //   const res = await getAllBillApi()
+  //   setOrder(res.data)
+  // }
+  // const ctx = useOrder();
+
+  // const { order } = ctx;
+  // console.log(order);
+  const [billOrder, setBillOrder] = useState();
+  useEffect(() => {
+    const getSlipPayment = async () => {
+      const result = await getAllBillApi();
+      console.log(result.data);
+      setBillOrder(result.data.billOrder);
+    };
+    getSlipPayment();
+  }, []);
+  if (!billOrder) {
+    return <></>;
+  }
+
   return (
     <>
       <HeaderCart />
@@ -10,8 +35,8 @@ export default function orderStatus() {
         className="py-16 flex justify-center items-center
     bg-gradient-to-br from-purple-700 to-amber-700"
       >
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-5">
-          <form className="space-y-6" action="#">
+        <div className="w-full h-[400px] max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-5">
+          <form>
             <div className="flex justify-center">
               <span className="bg-clip-text text-transparent font-bold bg-gradient-to-r from-yellow-500 via-pink-500 to-purple-500 text-2xl">
                 LALA
@@ -19,58 +44,64 @@ export default function orderStatus() {
                 Shop
               </span>
             </div>
-            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            <h5 className="text-md font-medium text-gray-900 dark:text-white">
               LALA SHOP Bill
             </h5>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Order Id
-              </label>
+            <div className="m-auto">
+              <tbody>
+                <tr className="border-b  dark:border-neutral-500 px-10">
+                  <td className="whitespace-nowrap   pr-48 py-2 font-normal text-sm">
+                    Order Id:
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm">
+                    {billOrder.id}
+                  </td>
+                </tr>
+                <tr className="border-b  dark:border-neutral-500 px-10">
+                  <td className="whitespace-nowrap  pr-48 py-2 font-normal text-sm">
+                    User Id:
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-2 font-normal text-sm">
+                    {billOrder.userId}
+                  </td>
+                </tr>
+              </tbody>
             </div>
-            <div className="flex items-start">
-              <label
-                htmlFor="remember"
-                className=" text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                User Id
-              </label>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Product Name
-              </label>
-            </div>
-            {/* <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Product Id
-              </label>
-            </div> */}
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Product Price
-              </label>
-            </div>
-
-            <div className="flex items-start">
-              <a
-                href="#"
-                className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-              >
-                Total Price:
-              </a>
-            </div>
+            <thead className="bg-white border-b">
+              <tr>
+                <th
+                  scope="col"
+                  className="text-sm font-medium text-gray-900 pr-16 py-2 text-left"
+                >
+                  Product Name
+                </th>
+                <th
+                  scope="col"
+                  className="text-sm font-medium text-gray-900 pl-6 py-2 text-left"
+                >
+                  Product Price
+                </th>
+              </tr>
+            </thead>
+            <hr />
+            <tbody>
+              <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="pr-32 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {billOrder.productName}
+                </td>
+                <td className="pl-6 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {billOrder.productPrice}
+                </td>
+              </tr>
+              <tr>
+                <td className="pr-32 py-2  text-sm text-blue-700 hover:underline dark:text-blue-500">
+                  Total Price:
+                </td>
+                <td className="pl-6 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {/* {total} */}123 B
+                </td>
+              </tr>
+            </tbody>
           </form>
         </div>
       </div>
